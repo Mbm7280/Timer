a2fd6d11592fd6664fe2132a371fc1c4dd314abd
Mbm7280 <Mbm1216@163.com>
2021-11-01 22:05:06 +0800

    Create XxlJobConfiguration.java

---
+++ src/main/java/xxl_job/task/config/XxlJobConfiguration.java
@@ -0,0 +1,51 @@
+package xxl_job.task.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @className XxlJobConfiguration
+ * @author bangmin.mai
+ * @description xxlJob配置类
+ * @updateTime 2021/10/22 23:20
+ * @version 1.0
+ */
+@Configuration
+public class XxlJobConfiguration {
+
+    //  1、读取配置文件信息
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+    @Value("${xxl.job.executor.appname}")
+    private String appName;
+    @Value("${xxl.job.executor.ip}")
+    private String ip;
+    @Value("${xxl.job.executor.port}")
+    private int port;
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+    @Value("${xxl.job.executor.logpath}")
+    private String logPath;
+    @Value("${xxl.job.executor.logretentiondays}")
+    private int logRetentionDays;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor(){
+        // 创建 XxlJobSpringExecutor 执行器
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appName);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        // 返回
+        return xxlJobSpringExecutor;
+    }
+
+
+
+}
